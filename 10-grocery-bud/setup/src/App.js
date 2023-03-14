15b88ddbefc1e10bb80c1fcea0ccc831136406ea
DevBakhtiyor <bakhtiyor.dev.it@gmail.com>
2023-03-14 18:38:08 -0400

    Add Remove Item function

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -35,11 +35,15 @@ function App() {
     showAlert(true, 'danger', 'empty list');
     setList([]);
   };
+  const removeItem = id => {
+    showAlert(true, 'danger', 'empty list');
+    setList(list.filter(item => item.id !== id));
+  };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -56,7 +60,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
